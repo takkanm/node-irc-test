d31aea81394a1acdbc6976865c67238f4d153da8
takkanm <takkanm@gmail.com>
2011-11-05 17:33:58 +0900

    imp echo back

--- irc_client.js
+++ irc_client.js
@@ -8,9 +8,15 @@ client.addListener('error', function(message) {
 	console.error('ERROR: %s: %s', message.command, message.args.join(' '));
 });
 
+var toMyMessageRegexp = new RegExp("^" + settings.irc.nick + ": ");
+
 client.on('message', function(from, to, message) {
-	console.info(from + ' ' + to);
-	client.send('NOTICE', to, 'hi');
+	console.info(from + ' ' + to + ' ' + message);
+	if (message.match(toMyMessageRegexp)) {
+		setTimeout(function() {
+			client.send('NOTICE', to, message.replace(toMyMessageRegexp, ''));
+		}, 500);
+	}
 });
 
 
